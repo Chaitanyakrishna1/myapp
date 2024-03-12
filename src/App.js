c9ddccab07046c21e5fe613c5ec39b5915bf87d3
chaitanya <krishnachaitanya@lyrostech.com>
2024-03-12 18:35:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './Header';
+import Box1 from './Box1';
+import Box2 from './Box2';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+           <div><h1><Header/> </h1> </div>  
+  
+      <div>
+           <div><Box1/></div>
+            <div><Box2/> </div>
+       </div>
+
+       <div> </div>
+
+
+      </div>
+     
   );
 }
 
-export default App;
+
